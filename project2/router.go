9ad9255cd7d77791640540612a7a465f74261ab8
Carter Weinberg <weinbergcarter@gmail.com>
2019-09-26 15:44:32 -0400

    Still figuring out net.dial issues

--- project2/router.go
+++ project2/router.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	//TODO, get proper command line parsing and then pass in the proper ip as the
-	// second argument to net.Dial()
-
-	//Look at the example code for how the proffessor parsed the command line args.
-
-	//Basic argument parsing
-	var port string
-	var ip string
-	flag.StringVar(&port, "p", "27993", "The port to connect to")
-	flag.StringVar(&ip, "ip", "", "The ip address to connect to")
-	flag.Parse()
+	ip := make([]string, len(os.Args))
+	port := make([]string, len(os.Args))
+	for i, network := range os.Args[1:] {
+		split := strings.Split(network, "-")
+		ip[i] = split[0]
+		port[i] = split[1]
+		//TODO do some argument testing to make sure user puts in correct fields
+		//TODO Open up len(os.args) many domain sockets and listen to them at the same time
+		//This requires goroutines or polling?
+	}
 
 	// Something is wrong here but I am not sure what.
-	conn, err := net.Dial("unixpacket", "/tmp/echo.sock")
+	conn, err := net.Dial("unixpacket", "./"+ip[0])
 
 	if err != nil {
 		println("Error message for connection")
@@ -30,7 +30,12 @@ func main() {
 	}
 
 	//This reads the TCP servers response.
-	message, err := bufio.NewReader(conn).ReadString('\n')
+	reader := bufio.NewReader(conn)
+	if reader == nil {
+		println("reader did not find anything")
+	}
+
+	message, err := reader.ReadString('\n')
 	if err != nil {
 		println("Error message for message")
 		fmt.Println(err)
