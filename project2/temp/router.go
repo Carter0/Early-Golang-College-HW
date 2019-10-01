c6a9dc432d2105fab78b5469d37131f9588ed7c5
Carter Weinberg <weinbergcarter@gmail.com>
2019-10-01 10:22:17 -0400

    adding goroutines

--- project2/temp/router.go
+++ project2/temp/router.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net"
 	"strings"
 )
+
+func handleConnection(conn net.Conn) {
+	//TODO, figure out how to read in json in golang.
+	reader := bufio.NewReader(conn)
+	if reader == nil {
+		println("reader did not find anything")
+	}
+
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		println("Error message for message")
+		fmt.Println(err)
+	}
+
+	print(message)
+}
 
 func main() {
 
@@ -14,48 +31,27 @@ func main() {
 
 	args := flag.Args()
 
+	var networkType string
+	if *useUnix {
+		networkType = "unix"
+	} else {
+		networkType = "unixpacket"
+	}
+
 	ip := make([]string, len(args))
 	port := make([]string, len(args))
 	for i, network := range args {
 		split := strings.Split(network, "-")
 		ip[i] = split[0]
 		port[i] = split[1]
+
+		//Open sockets and start listening.
+		conn, err := net.Dial(networkType, "./"+ip[i])
+		if err != nil {
+			panic(err)
+		}
+
+		go handleConnection(conn)
+
 	}
-	var network string
-	if *useUnix {
-		network = "unix"
-	} else {
-		network = "unixpacket"
-	}
-
-	//TODO do some argument testing to make sure user puts in correct fields
-	//TODO Open up len(os.args) many domain sockets and listen to them at the same time
-	//This requires goroutines or polling?
-
-	// Something is wrong here but I am not sure what.
-	conn, err := net.Dial(network, "./"+ip[0])
-
-	if err != nil {
-		println("Error message for connection")
-		fmt.Printf("%+v\n", err)
-	}
-
-	fmt.Printf("%+v\n", conn)
-
-	//TODO, figure out how to read in json in golang.
-	//Not sure if the reader is a good way to do it
-
-	// //This reads the TCP servers response.
-	// reader := bufio.NewReader(conn)
-	// if reader == nil {
-	// 	println("reader did not find anything")
-	// }
-
-	// message, err := reader.ReadString('\n')
-	// if err != nil {
-	// 	println("Error message for message")
-	// 	fmt.Println(err)
-	// }
-
-	//print(message)
 }
